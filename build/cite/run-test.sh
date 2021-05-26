3b3f65ce4647c48322862c958791b9ce55c9f5f4
Asdrubal Gonzalez <agpenton@gmail.com>
2021-05-26 14:11:20 +0400

    testing the logs, 8 try

--- build/cite/run-test.sh
+++ build/cite/run-test.sh
@@ -32,9 +32,9 @@ _show_logs() {
 #  fi
 #}
 _parse_logs(){
-  grep -R $TE_LOG_DIR/s0001 "Failed"
+  glogs=$(_show_logs | grep -iv "Failed")
   local grep_exit_code=$?
-  if [ "$grep_exit_code" -ne "0" ]; then
+  if ! glogs; then
       echo "Failed tests found in logs! (grep exit code: $grep_exit_code)" >&2
       return 3
   else
