147b728227a3f35667d92909d0bf929fb464dfa6
Asdrubal Gonzalez <agpenton@gmail.com>
2021-05-28 15:28:37 +0400

    testing logs try 31b

--- build/cite/run-test.sh
+++ build/cite/run-test.sh
@@ -86,11 +86,11 @@ echo "Parsing the logs"
   _parse_logs
 
 echo "The Result is $?"
-#  if [ "$?" -ne "0" ]; then
+  if [ "$?" -ne "0" ]; then
 #  if [ "$rc" -eq "0" ]; then
-#      echo "The log shows a failed test!" >&2
-#      rc=3
-#  fi
+      echo "The log shows a failed test!" >&2
+      rc=3
+  fi
 
   echo $rc
   return $rc
