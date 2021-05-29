3a34f3d66702763d0ea9d85659d034c43acb0114
Asdrubal Gonzalez <agpenton@gmail.com>
2021-05-29 11:49:36 +0400

    al 02

--- build/cite/run-test.sh
+++ build/cite/run-test.sh
@@ -21,7 +21,7 @@ _show_logs() {
 }
 
 _parse_logs(){
-  _show_logs | grep -iv "Failed"
+  _show_logs | grep -w "Failed"
   local grep_exit_code=$?
   if [ "$grep_exit_code" -eq "0" ]; then
       echo "Failed tests found in logs! (grep exit code: $grep_exit_code)" >&2
