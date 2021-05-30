d142ceff1287fb619e3a534878a5c3a63faade7a
Asdrubal Gonzalez <agpenton@gmail.com>
2021-05-30 20:37:16 +0400

    al 29

--- build/cite/run-test.sh
+++ build/cite/run-test.sh
@@ -27,7 +27,7 @@ _parse_logs(){
 #  if [ "$(_show_logs | grep -e "Failed")" != "0" ]; then
 #  _show_logs | grep -w "Failed" && echo "Failed tests found" exit 1 || echo "No Failed tests found in logs" exit 0
   # shellcheck disable=SC2092
-  if [ "$(`_show_logs` | grep -i -w "Failed")" != "0" ]; then
+  if [ "$(`_show_logs` | grep -i -w 'Failed')" != "0" ]; then
 #  if [ "$(grep -w "Failed" `_show_logs`)" = "0" ]; then
       grep_exit_code=$?
 #      echo "Failed tests found in logs! (grep exit code: $grep_exit_code)" >&2
